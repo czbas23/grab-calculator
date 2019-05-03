6ac72d7111ec1088a2f0db2001031e81b23422b1
Nattakit <czbas23@gmail.com>
2019-05-03 16:37:07 +0700

    Add file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-grab-calculator
+# grab-calculator
+
+### Exemple
+  - php grab_calculator.php -p 100,70,40,50,60,80,60,60,60 -d 125 -s 10
+  - php grab_calculator.php -p CzBas/100+20,70+10,40,50,60,80,60,60,60 -d 125 -s 10
---
+++ grab_calculator.php
@@ -0,0 +1,46 @@
+<?php
+// Exemple: php grab_calculator.php -p 100,70,40,50,60,80,60,60,60 -d 125 -s 10
+// Exemple: php grab_calculator.php -p Bas/100+20,70+10,40,50,60,80,60,60,60 -d 125 -s 10
+
+$options = getopt("d:p:s:c:");
+
+function getPrice($item) {
+  $explode_price = explode('/', $item);
+  if (count($explode_price) == 1) {
+    $name = 'Null';
+    $price = $explode_price[0];
+  } else {
+    $name = $explode_price[0];
+    $price = $explode_price[1];
+  }
+  $sum_price = array_sum(explode('+', $price));
+  return [$name, $price, $sum_price];
+}
+
+$prices = explode(',', $options['p']);
+$count = count($prices);
+$total_price = array_reduce($prices, function($carry, $item) {
+  list($name, $price, $sum_price) = getPrice($item);
+  $carry += $sum_price;
+  return $carry;
+});
+$discount = (float) $options['d'];
+$shipping = (float) $options['s'];
+
+$divide_shipping = round($shipping / $count, 2);
+
+$hr = str_repeat('-', 85) . "\n";
+print $hr;
+print "# Person $count\n";
+print "# Total price $total_price\n";
+print "# Shipping $shipping\n";
+print "# Shipping divide $divide_shipping\n";
+print $hr;
+foreach ($prices as $item) {
+  list($name, $price, $sum_price) = getPrice($item);
+  $cal_discount = round(($sum_price / $total_price * $discount), 2);
+  $balance = number_format($sum_price - $cal_discount + $divide_shipping, 2);
+  print "# $name\t\tPrice $sum_price\t\tDiscount " . number_format($cal_discount, 2) . "\t\tBalance $balance\n";
+  print $hr;
+}
+?>
